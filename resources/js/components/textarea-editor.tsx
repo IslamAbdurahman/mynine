71a576c82295380e51c57d668205f57782f5ddf5
islamabdurahman <abdurahmanislam304@gmail.com>
2026-03-29 23:39:53 +0500

    fix: resolve TinyMCE license key warning by adding license_key to init config

--- resources/js/components/textarea-editor.tsx
+++ resources/js/components/textarea-editor.tsx
@@ -108,6 +108,7 @@ export default function TextareaEditor({
                 value={value}
                 onEditorChange={(content) => onChange(content)}
                 init={{
+                    license_key: 'gpl',
                     base_url: '/vendor/tinymce',
                     suffix: '.min',
                     height,
